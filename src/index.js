848ef3a4c692e706b6a2d87bebd938926cda5cdd
AndressaSRodrigues <srandressa2@gmail.com>
2023-06-06 12:30:15 -0400

    Commit first part isValid

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import validator from './validator.js';
 
+const validateButton = document.getElementById('validate-button');
+
+validateButton.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const creditCardNumber = document.getElementById('credit-card-number').value;
+
+    validator.isValid(creditCardNumber);
+    
+
+    
+});
+
+
 console.log(validator);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,31 @@
 const validator = {
-  // ...
-};
+
+    isValid: function(creditCardNumber) {
+
+        let reversedNumber = creditCardNumber.split('').reverse();
+
+        console.log(reversedNumber);
+
+        for (let i = 1; i < reversedNumber.length; i += 2) {
+            
+            let duplicated = reversedNumber[i] *= 2;
+
+            if (duplicated > 9){
+                duplicated -= 9;
+            }
+            
+            console.log(duplicated);
+        }
+
+        /* for (let i = 0; i < reversedNumber.length; i += 2) {
+
+            let otherDigits = reversedNumber[i];
+
+            console.log(otherDigits);
+
+        }*/
+}
+}
+
 
 export default validator;
