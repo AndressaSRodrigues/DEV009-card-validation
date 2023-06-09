ca4d80e74d333734edc41014e8fb8a2268e1c6c3
AndressaSRodrigues <srandressa2@gmail.com>
2023-06-08 23:33:21 -0400

    Required and pattern input updated

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import validator from './validator.js';
 
-const validateButton = document.getElementById('validate-button');
+const validateInfo = document.getElementById('validate-info');
 
-validateButton.addEventListener('click', (e) => {
+validateInfo.addEventListener('submit', (e) => {
   e.preventDefault();
 
   const creditCardNumber = document.getElementById('credit-card-number').value;
 
   if (validator.isValid(creditCardNumber)) {
-    document.getElementById('validate-message').innerHTML = `Your credit card ${validator.maskify(creditCardNumber)} is valid. \n Your booking has been confirmed.`;
+    document.getElementById('validate-message').innerHTML = `Your credit card ${validator.maskify(creditCardNumber)} is valid.<br> Your booking has been confirmed.`;
+    validateInfo.reset();
   } else {
     document.getElementById('validate-message').innerHTML = 'We could not validate your card.';
   }
 
-  
 
 });
